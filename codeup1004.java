b26a12409ccc34dd893dfc05abea07cd030a40db
jihye.song <jiihye.song@gmail.com>
2020-05-21 17:46:42 +0900

    Add files via upload

---
+++ codeup1004.java
@@ -0,0 +1,12 @@
+public class codeup1004 {
+    /**
+     * 1004 : [기초-출력] 출력하기04(설명)
+     * 2019. 12. 15
+     * sjh
+     * **/
+
+    public static void main(String arg[]) {
+
+        System.out.println("\'Hello\'");
+    }
+}
