9e5d3596a20ec244fbdc0607d9e7875a277e5efd
jihye.song <jiihye.song@gmail.com>
2020-05-28 17:31:16 +0900

    Add files via upload

---
+++ codeup1005.java
@@ -0,0 +1,12 @@
+public class codeup1005 {
+    /**
+     * 1005 : [기초-출력] 출력하기05(설명)
+     * 2019. 12. 15
+     * sjh
+     * **/
+
+    public static void main(String arg[]) {
+
+        System.out.println("\"Hello World\"");
+    }
+}
