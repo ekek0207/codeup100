76b0e61c9e389d1cb5bd33436fe943d289812a6a
jihye.song <jiihye.song@gmail.com>
2020-05-31 16:20:46 +0900

    Add files via upload

---
+++ codeup1007.java
@@ -0,0 +1,30 @@
+public class codeup1007 {
+    /**
+     * 1007 : [기초-출력] 출력하기07(설명)
+     * 2019. 12. 15
+     * sjh
+     * **/
+
+    public static void main(String arg[]) {
+
+        /**
+         * 윈도우 운영체제의 파일 경로를 출력하는 연습을 해보자.
+
+         파일 경로에는 특수문자들이 포함된다.
+
+         다음 경로를 출력하시오.
+
+         "C:\Download\hello.cpp"
+         (단, 큰따옴표도 함께 출력한다.)
+
+         참고
+         printf( ) 함수로 백슬래시 문자 \ 를 출력하기 위해서는 \\ 로 출력해야 한다.
+
+         예시
+         printf("\\");
+
+         */
+
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+    }
+}
