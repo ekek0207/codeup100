3cf6f475c2a30f40db6624f6b6b17c2d44b23c40
jihye.song <jiihye.song@gmail.com>
2020-06-23 20:17:08 +0900

    Add files via upload

---
+++ codeup1251.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class codeup1251 {
+    /**
+     * 1251 : 1 부터 100까지 출력하기
+     * 2020. 01. 05.
+     * sjh
+     * **/
+
+    public static void main(String arg[]) {
+
+        /**
+         * 반복문 연습용 예제입니다.
+         * 입력은 없습니다.
+         * 1부터 100까지 공백으로 띄워 하나씩 출력하세요.
+         */
+        for(int i=1;i<101;i++){
+            System.out.print(i+" ");
+
+        }
+    }
+}
